a1e87787576452fd5911d784827f03f215964a6c
D4V1DEB <davidespinozabarrios@gmail.com>
2026-07-19 22:17:02 -0500

    se agrego los 3 experimentos

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,12 @@
 #include <ctime>
 
 #include "experiment1.hpp"
+#include "experiment2.hpp"
+#include "experiment3.hpp"
 
 // Funcion para medir tiempo (milisegundos de CPU, portable en Linux/WSL).
 // Usa clock() en vez de tiempo de pared: estable bajo carga de la maquina.
-inline double elapsed(bool reset = false) {
+double elapsed(bool reset = false) {
     static clock_t start = clock();
     if (reset) start = clock();
     return (1000.0 * double(clock() - start)) / double(CLOCKS_PER_SEC);
@@ -16,5 +18,7 @@ double elapsed() {
 
 int main() {
     runExperiment1();
+    runExperiment2();
+    runExperiment3();
     return 0;
 }
